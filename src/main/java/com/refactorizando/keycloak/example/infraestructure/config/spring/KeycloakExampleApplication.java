b402d4e75b727700263e9acd97b4c3fa5b8353fb
refactorizando <refactorizando.web@gmail.com>
2021-01-14 12:33:05 +0100

    add correct package

--- src/main/java/com/refactorizando/keycloak/example/infraestructure/config/spring/KeycloakExampleApplication.java
+++ src/main/java/com/refactorizando/keycloak/example/infraestructure/config/spring/KeycloakExampleApplication.java
@@ -3,8 +3,7 @@ package com.refactorizando.keycloak.example.infraestructure.config.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.beesion.example.keycloak.infraestructure")
-
+@SpringBootApplication(scanBasePackages = "com.refactorizando.example.keycloak.infraestructure")
 public class KeycloakExampleApplication {
 
 	public static void main(String[] args) {
